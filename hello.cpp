ea46777a74c218300985c4e50b2fc832bbc06d6a
CharlesFord120 <charlieford130@gmail.com>
2019-08-28 09:37:57 -0600

    Retrying the commit.

--- hello.cpp
+++ hello.cpp
@@ -7,8 +7,8 @@ int main()
 	cout << "# **The Life Of Charles Abriged** " << endl;
 	cout << "Hello my name is Charles Ford." << endl;
 	cout << " I enjoy __Hockey__ and Computers.  I have ADHD and two knees that are trying their best" << endl;
-	cout << " I have no pets but do enjoy animals. Here is a picture of a bunny! " << endl;
-	cout << " ! [Image of Bunny](bunny.jpg)";
+	cout << " I have no pets but do enjoy animals. Here is a picture of a bunny! " << endl << endl;
+	cout << "![Image of Bunny](bunny.jpg)";
 	
 	return 0;
 
--- hello.md
+++ hello.md
@@ -2,4 +2,5 @@
 Hello my name is Charles Ford.
  I enjoy __Hockey__ and Computers.  I have ADHD and two knees that are trying their best
  I have no pets but do enjoy animals. Here is a picture of a bunny! 
- ! [Image of Bunny](bunny.jpg)
+
+![Image of Bunny](bunny.jpg)
