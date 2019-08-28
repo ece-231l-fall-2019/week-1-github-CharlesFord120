e0e8981b1a3eb706b769ac1685a60fba99beede7
CharlesFord120 <charlieford130@gmail.com>
2019-08-28 09:34:01 -0600

    My first Commit

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "# **The Life Of Charles Abriged** " << endl;
+	cout << "Hello my name is Charles Ford." << endl;
+	cout << " I enjoy __Hockey__ and Computers.  I have ADHD and two knees that are trying their best" << endl;
+	cout << " I have no pets but do enjoy animals. Here is a picture of a bunny! " << endl;
+	cout << " ! [Image of Bunny](bunny.jpg)";
+	
+	return 0;
+
+}
+
---
+++ hello.md
@@ -0,0 +1,5 @@
+# **The Life Of Charles Abriged** 
+Hello my name is Charles Ford.
+ I enjoy __Hockey__ and Computers.  I have ADHD and two knees that are trying their best
+ I have no pets but do enjoy animals. Here is a picture of a bunny! 
+ ! [Image of Bunny](bunny.jpg)
